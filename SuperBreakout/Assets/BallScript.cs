6e7cdcde212167b0ffce21d50f40b6d534bea7b6
MosiahF <17077225@mukilteo.wednet.edu>
2021-06-14 02:00:30 -0700

    Added Framework from tutorial mostly working

---
+++ SuperBreakout/Assets/BallScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Respawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Respawn()
+    {
+        transform.position = Vector3.zero;
+        GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle.normalized * speed;
+    }
+}
